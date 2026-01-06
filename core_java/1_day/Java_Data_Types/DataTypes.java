e3819418b8ca54956e7ee641cfe499fd1f1ba41b
devmulkalwar <devmulkalwar95@gmail.com>
2026-01-06 10:28:40 +0530

    Java DataTypes code added

---
+++ core_java/1_day/Java_Data_Types/DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes {
+    public static void main(String[] args) {    
+        //numeric data types
+        int num1 = 10;
+        System.out.println("Value of num1 is : "+ num1);
+        float num2 = 10.1f;
+        System.out.println("Value of num2 is : "+ num2);
+        double num3 = 10.2222d;
+        System.out.println("Value of num3 is : "+ num3);
+        long num4 = 100000L;
+        System.out.println("Value of num4 is : "+ num4);
+        byte num5 = 4;
+        System.out.println("Value of num5 is : "+num5);
+        short num6 = 30000;
+        System.out.println("Value of num6 is : "+ num6);
+
+        //character data type
+        char char1 = 'a';
+        System.out.println("Value of char1 is : "+char1);
+
+        //boolean data type
+        boolean isValid = true;
+        System.out.println("Value of isValid is : "+ isValid);
+    }
+}
